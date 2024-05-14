7eec06224ffad650395aab2eb749cd6b0f07b8b5
thao.vu <thaovu@miu.edu>
2024-05-14 08:54:31 -0500

    added ws4

---
+++ workshop04/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, View} from 'react-native';
+import Constants from 'expo-constants';
+
+
+export default function App() {
+
+  return (
+      <View style={styles.container}>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Constants.statusBarHeight,
+  },
+});
